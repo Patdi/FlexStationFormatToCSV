0c1f4bfd6c6301f2c7b2e66262e7865e62702b4e
Patdi <25618699+Patdi@users.noreply.github.com>
2026-02-28 23:22:48 -0800

    Add files via upload
    Ported to Python3

--- FlexStationFormatToCSV.py
+++ FlexStationFormatToCSV.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+i#!/usr/bin/env python3
 
 #Converts a Molecular Devices FlexStation 3 Microplate Reader .csv file to a easier to use format
 #On the command line run: cat "your_file".csv | FlexStationFormatToCSV.py > "your_output".csv
@@ -7,54 +7,47 @@
 import re
 import sys
 
-def pretty_print(s, plate_name, well_co, absorbance, temp, wave):
-	for i in range(len(well_co)):
-		print str(s+i) + "," + str(plate_name)  + "," + str(well_co[i])  + "," + str(absorbance[i]) + "," + str(temp) + "," + str(wave)
-	
-n = 0 #current line number
-abs_row = 0 #line number of abs row
-sample = 1 #sample number
-wbool = False #More one wavelenght
-wpos = 0 #position in list of wavelenght
+def pretty_print(start_idx, plate_name, well_co, absorbance, temp, wave):
+    for i in range(len(well_co)):
+        print(
+            f"{start_idx + i},{plate_name},{well_co[i]},{absorbance[i]},{temp},{wave}"
+        )
 
-#header
-print "Sample, Plate, Well, Absorbance, Temperature, Wavelenght"
+n = 0 
+abs_row = 0
+sample = 1
+wbool = False
+wpos = 0 
 
-#parse file, when key words found, split line on comma
+print("Sample, Plate, Well, Absorbance, Temperature, Wavelenght")
+
 for raw_line in sys.stdin:
-	line = raw_line.strip()
-	#block number
-	if re.search('##BLOCKS=', line):
-		block = line.split(",")[0].split(" ")[1]
-	elif line.replace(",","").rstrip() == False:
-		continue
-	#time
-	elif re.search('Plate:', line):
-		#plate = line.split(",")[1].replace("t","")
-		plate = line.split(",")[1]
-		#wavelenght list
-		wavelenght = line.split(",")[15].split(" ")
-		if len(wavelenght) >= 2:
-			wbool = True
-	#well nums and find first abs_row
-	elif re.search('Temperature', line):
-		well = line.split(",")[2:]
-		abs_row = n + 1 
-	#temperature
-	elif abs_row != 0 and abs_row == n:
-		temperature = line.split(",")[1]
-		abs = line.split(",")[2:]
-		pretty_print(sample, plate, well, abs, temperature, wavelenght[wpos])
-		sample = sample + len(well)
-		wpos += 1
-	elif wbool == True and re.search(temperature, line):
-		temperature = line.split(",")[1]# prob delete later
-		abs = line.split(",")[2:]
-		pretty_print(sample, plate, well, abs, temperature, wavelenght[wpos])
-		sample = sample + len(well)
-		wpos += 1
-	#end of record print
-	elif re.search('~End', line):
-		temp_bool = False
-		wpos = 0
-	n += 1
+    line = raw_line.strip()
+    if re.search(r"##BLOCKS=", line):
+        block = line.split(",")[0].split(" ")[1]
+    elif not line.replace(",", "").rstrip():
+        continue
+    elif re.search(r"Plate:", line):
+        plate = line.split(",")[1]
+        wavelenght = line.split(",")[15].split(" ")
+        if len(wavelenght) >= 2:
+            wbool = True
+    elif re.search(r"Temperature", line):
+        well = line.split(",")[2:]
+        abs_row = n + 1
+    elif abs_row != 0 and abs_row == n:
+        temperature = line.split(",")[1]
+        absorbances = line.split(",")[2:]
+        pretty_print(sample, plate, well, absorbances, temperature, wavelenght[wpos])
+        sample += len(well)
+        wpos += 1
+    elif wbool is True and re.search(re.escape(temperature), line):
+        temperature = line.split(",")[1] 
+        absorbances = line.split(",")[2:]
+        pretty_print(sample, plate, well, absorbances, temperature, wavelenght[wpos])
+        sample += len(well)
+        wpos += 1
+    elif re.search(r"~End", line):
+        wpos = 0
+
+    n += 1
